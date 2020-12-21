f67881cb8a2541915bd923a900199b247f869ace
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2020-12-21 11:45:00 -0300

    boolean concepts

---
+++ basics/boolean.py
@@ -0,0 +1,12 @@
+# we can use True and False
+1 == 1 # true
+# operators
+(1 == 1) and (2 < 5) # both have to be true
+(1 == 1) or (2 < 0) # at least one true
+not(2 < 5)
+# deny an expression
+not(1 == 2)
+# false values 0j is a complex number
+None, 0, 0.0, 0j, "", [], (), {}
+
+bool() # evaluate the variablles
