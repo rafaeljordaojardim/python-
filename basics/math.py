acc709b7f9785f3e794da0c377a18b136fc2aac3
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2020-12-21 11:37:44 -0300

    math basics

---
+++ basics/math.py
@@ -0,0 +1,19 @@
+# This is to learn about math operators
+
+# intergers, floating-point numbers and complex numbers
+num1 = 10
+num2 = 2.5
+type(num1) # return the type class of variable
+# / * + - 
+# integer division 5 // 2 
+# 5 % 2 = 1
+# 4 ** 2 pow function
+# >, <, <=, >=, ==, !=
+
+# functions
+int(1.7) # returns 1
+float(2) # return 2.0
+abs(5) # distance of the number 5 from 0
+max(1, 2)
+min(1, 2)
+pow(3, 2)
