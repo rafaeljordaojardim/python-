7043949dcfba4e256f449a26787be410ba5a48d2
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2020-12-21 10:11:51 -0300

    about variables

---
+++ basics/variables.py
@@ -0,0 +1,18 @@
+# we can put any type on a variable
+# the declaration is done automatically
+# it is case sensitive my_var is different of my_Var
+# we have to use = sign to attribute some value
+
+a = b = c = 10
+
+a, b, c = 1, 2, 3
+
+# id() it returns the local memoty where the value is stored
+# python keywords
+# False      await      else       import     pass
+# None       break      except     in         raise
+# True       class      finally    is         return
+# and        continue   for        lambda     try
+# as         def        from       nonlocal   while
+# assert     del        global     not        with
+# async      elif       if         or         yield
