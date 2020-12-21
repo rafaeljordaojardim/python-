1e4aeebb30fcdd9cb47b41adaf9fdd82e936d237
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2020-12-21 15:52:17 -0300

    conversioning of datatypes

---
+++ basics/conversion.py
@@ -0,0 +1,40 @@
+### conversion between types ###
+
+num = 2
+
+f = 2.5
+
+type(num)
+
+type(f)
+
+num2 = str(num)
+
+f2 = str(f)
+
+str1 = "5"
+
+int1 = int(str1)
+
+f3 = float(str1)
+
+num1 = 2
+
+f = float(num1)
+
+tuple1 = (1, 2, 3)
+list1 = list(tuple1)
+
+tup = tuple(list1)
+
+set1 = set(list1)
+
+num = 10
+
+num_bin = bin(num)
+
+num_hex = hex(num)
+
+bin_to_num = int(num_bin, 2)
+
+hex_to_num = int(num_hex, 16)
