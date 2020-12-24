a0e0719e37c577f5c71e29982e1a9c12471cd6c4
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2020-12-24 11:04:45 -0300

    best practices

---
+++ advanced/code-best-practice.py
@@ -0,0 +1,16 @@
+# Correct:
+
+# Aligned with opening delimiter.
+# foo = long_function_name(var_one, var_two,
+#                          var_three, var_four)
+
+# Add 4 spaces (an extra level of indentation) to distinguish arguments from the rest.
+def long_function_name(
+        var_one, var_two, var_three,
+        var_four):
+    print(var_one)
+
+# Hanging indents should add a level.
+# foo = long_function_name(
+#     var_one, var_two,
+#     var_three, var_four)
